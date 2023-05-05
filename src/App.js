d5c64da8fc5c52ac4655ede06c4a5849125ac477
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-05-04 21:38:48 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import freeCodeCamp from './imagenes/freecodecamp-logo.png'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+        className='freecodecamp-logo'
+        src={freeCodeCamp}
+        alt='Logo de Freecodecamp'
+        />
+
+      </div>
       
     </div>
   );
